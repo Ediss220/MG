1ebde9a3b59ba824e01265bf87778b9d9dfd978d
Ediss <eduard.issakov@gmail.com>
2022-06-28 12:38:29 -0400

    test

--- src/test/java/API_test.java
+++ src/test/java/API_test.java
@@ -28,4 +28,56 @@ public class API_test {
                 .log().body();
 
     }
+    @Test
+    public void SearchBoxAPI() {
+        given()
+                .param("api_key","41a6894ca93cb1c78657d9e799e164de")
+                .param("language","en-US")
+                .param("query","Mulan")
+                .get("https://api.themoviedb.org/3/search/movie")
+                .then().statusCode(200)
+                .body("", Matchers.notNullValue())
+                .log().body();
+
+    }
+    @Test
+    public void SearchBoxAPI() {
+        given()
+                .param("api_key","41a6894ca93cb1c78657d9e799e164de")
+                .param("language","en-US")
+                .param("query","Mulan")
+                .get("https://api.themoviedb.org/3/search/movie")
+                .then().statusCode(200)
+                .body("", Matchers.notNullValue())
+                .log().body();
+
+    }
+    @Test
+    public void SearchBoxAPI() {
+        given()
+                .param("api_key","41a6894ca93cb1c78657d9e799e164de")
+                .param("language","en-US")
+                .param("query","Mulan")
+                .get("https://api.themoviedb.org/3/search/movie")
+                .then().statusCode(200)
+                .body("", Matchers.notNullValue())
+                .log().body()
+                ("https://api.themoviedb.org/3/search/movie")
+                .then().statusCode(200)
+                .body("", Matchers.notNullValue())
+                .log().body();
+
+    }
+    @Test
+    public void SearchBoxAPI() {
+        given()
+                .param("api_key","41a6894ca93cb1c78657d9e799e164de")
+                .param("language","en-US")
+                .param("query","Mulan")
+                .get("https://api.themoviedb.org/3/search/movie")
+                .then().statusCode(200)
+                .body("", Matchers.notNullValue())
+                .log().body();
+
+    }
 }
